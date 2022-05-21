e38cbd75a2a4d8244031f3d129b7bae9758a7ea8
Majid Ali <67096621+Majidalee1@users.noreply.github.com>
2022-05-21 15:44:56 +0500

    added db configs

--- database/config.js
+++ database/config.js
@@ -6,18 +6,11 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: "bc4bf96ed23271",
+    password: "150f73bd",
+    database: "heroku_982fc4ceda5d5f5",
+    host: "eu-cdbr-west-02.cleardb.net",
     dialect: "mysql",
   },
 };
